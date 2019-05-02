761cc486183f052171cc0ebebb7a930354dc9971
daro <dario.siroki@gmail.com>
2019-05-03 00:09:40 +0200

    .

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const jwt = require("jsonwebtoken");
 const sequelize = require("./config/DatabaseHandler");
 require("dotenv").config();
 const server = express();
@@ -12,15 +13,27 @@ server.set("view engine", "handlebars");
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
+// Cookie parse middleware
+const cookieParser = require('cookie-parser')
+server.use(cookieParser());
+
 // API routes
 server.use("/api/users", require("./api/users"));
 
 // Handlebars routes
 server.use(express.static("public"));
-server.get("/", (req, res) =>
-  res.render("home", {
-    title: "Home"
-  })
+server.get("/", (req, res) => {
+    jwt.verify(req.cookies.session, process.env.SECRET, (e, data) => {
+      if(e){
+        console.log(e);
+      } else {
+        console.log(data)
+      }
+    })
+    res.render("home", {
+      title: "Home"
+    });
+  }
 );
 server.get("/user", (req, res) =>
   res.render("user", {
