06f5196a19d90fb236f42b84a3db2c7394509a77
Leonardo Gazdek <bot.denobot.deno@gmail.com>
2019-05-02 23:47:04 +0200

    Escapanje i optimizacije

--- app/api/users.js
+++ app/api/users.js
@@ -11,7 +11,8 @@ router.use(cookieParser());
 router.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  sequelize.query(`SELECT * from users where email='${email}' or username='${email}'`, {
+  sequelize.query("SELECT * from users where email=:email or username=:email LIMIT 1", {
+      replacements: {email: email},
       type: sequelize.QueryTypes.SELECT
     })
     .then(data=>{
@@ -33,15 +34,25 @@ router.post("/register", (req, res) => {
   const username = req.body.username;
   const email = req.body.email;
   const password = sha512(req.body.password);
-  sequelize.query(`SELECT * from users where email='${email}' or username='${username}';`, {
+  sequelize.query(`SELECT id from users where email=:email or username=:username LIMIT 1`, { // ne treba selectat sve kad je samo za provjeru
+      replacements: {
+          email: email,
+          username: username
+      },
       type: sequelize.QueryTypes.SELECT
     })
     .then(data=>{
       if(data.length) {
         res.status(403).json({msg: "Username or email already exists"});
       } else {
-        sequelize.query(`INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${password}');`)
-          .then(data=>{
+        sequelize.query(`INSERT INTO users (username, email, password) VALUES (:username, :email, :password)`, {
+            replacements: {
+              username: username,
+              email: email,
+              password: password
+            }
+        })
+        .then(data=>{
             jwt.sign({ "username": username }, process.env.SECRET, (err, token) => {
               res.cookie("session", token);
               res.json({msg: "Successfully logged in"});
